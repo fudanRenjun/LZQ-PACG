21bfd6d43bf44e70aed06240ea5e608a78f754c6
fudanRenjun <20211260002@fudan.edu.cn>
2024-12-25 16:34:20 +0800

    Update glaucom.py

--- glaucom.py
+++ glaucom.py
@@ -54,19 +54,3 @@ if st.button("Predict"):
         )
 
     st.write(advice)
-
-    # 计算并显示SHAP值
-    explainer = shap.TreeExplainer(model)
-    shap_values = explainer.shap_values(pd.DataFrame([feature_values], columns=feature_names))
-
-    # 根据预测结果生成并显示SHAP force plot
-    if predicted_class == 1:
-        shap.force_plot(explainer.expected_value[1], shap_values[1],
-                        pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)
-    else:
-        shap.force_plot(explainer.expected_value[0], shap_values[0],
-                        pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)
-
-    # 保存SHAP图并显示
-    plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)
-    st.image("shap_force_plot.png")
